f0bd22e74241d1688680b128d166fdc580746598
Jesse Nehemiah <nehemiahjess@gmail.com>
2024-06-06 12:24:12 +0300

    Made changes to the main.dart using my previous boilerplate for a news app

--- lib/main.dart
+++ lib/main.dart
@@ -36,3 +36,96 @@ class MainPage extends StatefulWidget {
   @override
   State<MainPage> createState() => _MainPageState();
 }
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = 0; // Initialize currentIndex
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HistMan"),
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          ),
+        ),
+        backgroundColor: Colors.greenAccent[400],
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.bar_chart_rounded),
+            tooltip: 'Progress',
+            onPressed: () {
+              // Handle account circle button press
+              // Navigate to the Progress screen
+              Navigator.pushNamed(context, '/progress');
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.grey[300]!, // Border color
+              width: 1.0, // Border width
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int newIndex) {
+            // Navigate to the selected screen based on index
+            switch (newIndex) {
+              case 0:
+                Navigator.pushNamed(context, '/home');
+                break;
+              case 1:
+                Navigator.pushNamed(context, '/search');
+                break;
+              case 2:
+                Navigator.pushNamed(context, '/alerts');
+                break;
+              case 3:
+                Navigator.pushNamed(context, '/account');
+                break;
+              case 4:
+                Navigator.pushNamed(context, '/progress');
+                break;
+            }
+          },
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.red,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications_active_rounded),
+                label: 'Alerts'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_rounded), label: 'Account'),
+          ],
+          elevation: 8,
+          unselectedLabelStyle:
+              TextStyle(fontWeight: FontWeight.normal, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
